1818cac5feaa8db56d1b68ac87ae9cab9438b9fc
shahebrahimiali <ali.sh.creative@gmail.com>
2025-11-10 09:40:24 +0000

    priliminary add

---
+++ dummy.py
@@ -0,0 +1,9 @@
+data=[2,3,5,7,11]
+
+for i,dt in enumerate(data):
+    print(f"Index: {i}, Value: {data}")
+
+print("--------------------------------------------------------------------")
+ali=["ali","veli","deli"]
+for i,x in enumerate(ali):
+    print(i, f"Name: {x}")
