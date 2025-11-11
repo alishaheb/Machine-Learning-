e03838c8ce00590b3b6f294b97c2c9aa86239131
shahebrahimiali <ali.sh.creative@gmail.com>
2025-11-11 23:55:20 +0000

    change lr to 0.05 and es400

--- Titanic/gencode.py
+++ Titanic/gencode.py
@@ -136,8 +136,8 @@ class MLProcessor:
         elif model_type == "xgboost":
             return XGBClassifier(
                 random_state=42,
-                n_estimators=300,
-                learning_rate=0.03,
+                n_estimators=400,
+                learning_rate=0.05,
                 max_depth=4,
                 n_jobs=-1,
                 eval_metric="logloss"
