e176a2dd41bca65faf69914310cc98a4db77a63d
shahebrahimiali <ali.sh.creative@gmail.com>
2025-11-19 23:45:19 +0000

    add genral

---
+++ genral.py
@@ -0,0 +1,13 @@
+import pandas as pd
+import numpy as np
+from sklearn.model_selection import train_test_split
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+from sklearn.pipeline import Pipeline
+from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
+from sklearn.metrics import classification_report, mean_squared_error
+from typing import Optional
+
+
+
