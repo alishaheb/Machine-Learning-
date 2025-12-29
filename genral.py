30cb00a9258592e03408ebe1300ac7f7cf98feb9
shahebrahimiali <ali.sh.creative@gmail.com>
2025-12-29 23:42:13 +0000

    phoshing add

--- genral.py
+++ genral.py
@@ -8,6 +8,10 @@ from sklearn.pipeline import Pipeline
 from sklearn.ensemble import RandomForestClassifier, RandomForestRegressor
 from sklearn.metrics import classification_report, mean_squared_error
 from typing import Optional
+
+
+
+RANDOMSTATE = 42
 IMPORTANT = 7  # Number of top features to display
 #dataframe=pd.DataFrame('')
 class MLProcessor:
