552cc387c7e1c78c92b9a9c05fac5b31354b69a3
shahebrahimiali <ali.sh.creative@gmail.com>
2026-03-01 23:59:50 +0000

    Add SHAP

--- Titanic/titanic.py
+++ Titanic/titanic.py
@@ -22,6 +22,7 @@ X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
 # =========================================================
+
 # 5. Preprocessing pipelines
 numeric_transformer = Pipeline(steps=[
     ("imputer", SimpleImputer(strategy="median")),
