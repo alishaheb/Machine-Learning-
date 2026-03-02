5dc038ab7c1822f1eae313f858aa64ccad4d2659
shahebrahimiali <ali.sh.creative@gmail.com>
2026-03-02 00:00:02 +0000

    Add SHAP

--- Titanic/titanic.py
+++ Titanic/titanic.py
@@ -21,6 +21,7 @@ dnumeric_cols = X.select_dtypes(include=["number"]).columns.tolist()
 X_train, X_test, y_train, y_test = train_test_split(
     X, y, test_size=0.2, random_state=42, stratify=y
 )
+
 # =========================================================
 
 # 5. Preprocessing pipelines
